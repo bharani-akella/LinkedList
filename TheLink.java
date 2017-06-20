e2c42e6b04043d2e019506967a9815ff5d5ef155
Bharani Akella <bharani0akella@gmail.com>
2017-06-20 18:21:25 +0530

    Add files via upload
    The main linked list

---
+++ TheLink.java
@@ -0,0 +1,13 @@
+package interview;
+import java.util.Scanner;
+public class TheLink {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         LinkToolKit tool=new LinkToolKit();
+         tool.showOptions();
+        
+	}
+
+}
+
